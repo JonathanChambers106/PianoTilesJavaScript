209f5eedde3e3a40b417aa080d2883c40fbbfc49
Glitch (wiry-plaid-valley) <none>
2021-05-12 20:01:51 +0000

    😽🏁 Checkpoint ./script.js:16176869/341

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
-/* global    background, createCanvas, ellipse, noFill, stroke, strokeWeight */
+/* global
+ *   background, createCanvas, ellipse, noFill, stroke, strokeWeight 
+ */
 
 // Content behind double slashes is a comment. Use it for plain English notes,
 // or for code that you want to temporarily disable.
@@ -15,6 +17,19 @@ function draw(){
 
   noFill();
   strokeWeight(5);
-  stroke(255, 0, 0);
+
+  stroke(10, 160, 255);
   ellipse(50, 50, 50, 50);
+
+  stroke(0, 0, 0);
+  ellipse(110, 50, 50, 50);
+
+  stroke(255, 0, 0);
+  ellipse(170, 50, 50, 50);
+
+  stroke(240, 210, 0);
+  ellipse(80, 80, 50, 50);
+
+  stroke(0, 180, 0);
+  ellipse(140, 80, 50, 50);
 }
