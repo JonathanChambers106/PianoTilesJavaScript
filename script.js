3cc93b03cdf6598c51df24103516cfaad7e9bdf5
Glitch (wiry-plaid-valley) <none>
2021-07-07 16:05:10 +0000

    🎇⛪️ Checkpoint ./script.js:16176869/2140

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
- *   background, createCanvas, ellipse, noFill, stroke, strokeWeight,
- *   loadImage, image, width, height, colorMode, noStroke, HSB, color,
- *   fill, ellipse, text, line, mouseX, mouseY
+ *  createCanvas, background, noFill, strokeWeight, stroke, ellipse
  */
 
-// We'll use variables for most of our colors in this code-along.
-let backgroundColor, color1, color2, textColor;
-
 function setup() {
-  // Canvas & color settings
   createCanvas(400, 400);
-  colorMode(HSB, 360, 100, 100);
-  noStroke();
-
-  // When used with only one argument, the color mode is greyscale.
-  // 0 is black and 100 is white.
-  backgroundColor = color(95);
-  textColor = color(20);
-  // When used with three arguments, the function takes, in this order:
-  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
-  //       is blue.
-  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
-  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
-  color1 = color(0, 80, 80);
-  color2 = color(200, 80, 80);
 }
 
 function draw() {
-  background(backgroundColor);
-  // Call the drawCenterLine function here to run the three lines of code
-  // contained in that function.
+  background(220);
 
+  // Brush settings
+  noFill();
+  strokeWeight(5);
 
-  // The red and blue circles:
-  fill(color1);
-  ellipse(100, 200, 50);
-  fill(color2);
-  ellipse(300, 200, 50);
-
-  // The grey circle and the text:
-  fill(textColor);
+  // Ring 1: Blue
+  stroke(10, 134, 205);
   ellipse(50, 50, 50);
-  text("Flip the switch", 20, 20);
-}
 
-function drawCenterLine() {
-  // This function will turn stroke on, draw the line, and then turn stroke
-  // back off.
-  // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
-  stroke(textColor);
-  line(200, 0, 200, 400);
-  noStroke();
+  // Ring 2: Yellow
+  stroke(255, 214, 0);
+  ellipse(80, 80, 50);
+
+  // Ring 3: Black
+  stroke(0, 0, 0);
+  ellipse(110, 50, 50);
+
+  // Ring 4: Green
+  stroke(33, 176, 76);
+  ellipse(140, 80, 50);
+
+  // Ring 5:
+  stroke(234, 30, 35);
+  ellipse(170, 50, 50);
 }
