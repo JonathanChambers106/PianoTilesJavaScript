79df1b9015be84d12273d02bd3da720e0a2e7e20
Glitch (wiry-plaid-valley) <none>
2021-07-30 19:04:42 +0000

    🌸👗 Checkpoint ./script.js:33131964/733

--- script.js
+++ script.js
@@ -3,38 +3,7 @@
  *  createCanvas, color, colorMode, strokeWeight, stroke, fill, HSB, noFill, background, ellipse, rect
  */
 
-// Content behind double slashes is a comment. Use it for plain English notes,
-// or for code that you want to temporarily disable.
 
-function setup(){
-  // Code here runs only once
-  createCanvas(800, 600)
-}
- // blue, yellow, black, green and red on a white background
-function draw(){
-  // Code here runs continuously
-  background(230)
 
-  
-  stroke(color(0, 0, 255));
-  strokeWeight(10);
-  ellipse(150, 130, 200, 200)
 
-  stroke('#222222');
-  strokeWeight(10);
-  ellipse(400, 130, 200, 200)
 
-  stroke('red');
-  strokeWeight(10);
-  ellipse(650, 130, 200, 200)
-  
-  stroke('rgb(0,255,0)');
-  strokeWeight(10);
-  ellipse(525, 230, 200, 200)
-  
-  stroke(255, 204, 0);
-  strokeWeight(10);
-  ellipse(275, 230, 200, 200)
-  
-  noFill()
-}
