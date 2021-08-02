70643c5d5b6aab0cdc498f236154a138920e7ddb
Glitch (wiry-plaid-valley) <none>
2021-08-02 14:20:36 +0000

    😋🚊 Checkpoint ./script.js:33134462/556

--- script.js
+++ script.js
@@ -6,3 +6,42 @@
 
 
 
+
+let WIDTH = 100;
+let HEIGHT = 150;
+
+let time;
+let score;
+let tiles = []
+function setup (){
+  createCanvas(401, 601);
+  
+  time = 0;
+  for (let i = 0; i < 4; i++ )
+    newRow();
+ 
+}
+
+function draw(){
+  background(51);
+  
+  for (let i = 0; i < tiles.length; i++){
+ 
+    let x = (i % 4) * WIDTH;
+    let y = ((3 -Math.floor(i / 4)) * HEIGHT);
+    
+    
+    
+    fill((tiles[i] === 0) ? 0 : 255)
+    rect(x, y, WIDTH, HEIGHT);
+  }
+}
+
+
+
+function newRow(){
+  let t = Math.floor(random(4));
+  
+  for(let i = 0; i < 4; i++)
+    tiles.unshift((t === i) ? 0 : 1);
+}
