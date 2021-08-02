d12f6cd9f491ec7c6a538de5218e83c5e182ded0
Glitch (wiry-plaid-valley) <none>
2021-08-02 10:21:46 +0000

    🌼💁 Checkpoint ./script.js:33134462/474

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
- *  createCanvas, color, colorMode, strokeWeight, stroke, fill, HSB, noFill, background, ellipse, rect
+ *  createCanvas, color, colorMode, strokeWeight, stroke, fill, HSB, noFill, background, ellipse, rect, time, i, noLoop, textSize, text, width, height, tiles, newRow, random, mouseY, mouseX
  */
-
 
 
 
