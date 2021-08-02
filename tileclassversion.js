f331c213a4ff43fc6b49a22132870791084c34f3
Glitch (wiry-plaid-valley) <none>
2021-08-02 18:15:34 +0000

    🙆👋 Checkpoint ./tileclassversion.js:33131964/1604:33134462/4:33859002/45

--- tileclassversion.js
+++ tileclassversion.js
@@ -3,27 +3,45 @@
  *  createCanvas, color, colorMode, strokeWeight, stroke, fill, HSB, noFill, WIDTH, HEIGHT, background, ellipse, rect, time, i, noLoop, textSize, text, width, height, tiles, newRow, random, mouseY, mouseX
  */
 
+let rectiles;
+
+
 function setup() {
   createCanvas(401, 601);
+  rectiles = [
+  new Tile(20, 80, 30, 60, 1)  
+  ]
 }
 
 function draw() {
   background(51);
-
-  class tile {
-    constructor(WIDTH, HEIGHT, x, y, velocity) {
-      this.WIDTH = 100;
-      this.HEIGHT = 150;
-      this.x = x;
-      this.y = this.HEIGHT;
-      this.velocity = 1;
-    }
+  for (let drawrectangle = 0; drawrectangle < rectiles.length; drawrectangle++){
+    rectiles[drawrectangle]
   }
 
-  this.draw = function(rectangle) {
-    noStroke()
-    fill(255, 204, 0);
-    rect(this.x, this.y, this.WIDTH, this.HEIGHT);
-    
-  };
 }
+
+class Tile {
+    constructor(width, height, x, y, velocity) {
+      this.width = 100;
+      this.height = 150;
+      this.x = x;
+      this.y = this.height;
+      this.velocity = 1;
+    }
+    drawrectangle() {
+    fill(255, 204, 0);
+    rect(this.x, this.y, this.width, this.height);
+  };
+  
+  
+  
+  
+  
+  }
+
+
+
+
+
+
