d1c22e9606110748a97c776d79687199e7f67f02
Glitch (wiry-plaid-valley) <none>
2021-08-03 17:45:41 +0000

    🚛⚾️ Checkpoint ./tileclassversion.js:33131964/52:33134462/111:33859002/3546

--- tileclassversion.js
+++ tileclassversion.js
@@ -10,14 +10,13 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   rectiles = [
     /*to do: change x's value to random position
-    *
-    */
+     *make tiles smaller
+     */
     new Tile(90, 300, 50, 50, 1),
     new Tile(90, 300, 250, 10, 1),
     new Tile(90, 200, 300, 350, 1),
-    new Tile(90, 200, 150, 450, 1),
+    new Tile(90, 200, 150, 450, 1)
   ];
-  
 }
 
 function draw() {
@@ -27,35 +26,36 @@ function draw() {
     drawrectangle < rectiles.length;
     drawrectangle++
   ) {
-   rectiles[drawrectangle].drawrectangle();
-   rectiles[drawrectangle].move(Tile);
+    rectiles[drawrectangle].drawrectangle();
+    rectiles[drawrectangle].move(Tile);
   }
+     hit = collideRectRect(90, 300, 100, 150, mouseX, mouseY, 50, 75);
 }
 
+function addScore() {}
 
-function addScore(){}
-
-
-function mousePressed(){
-  for (let i = 0; i < rectiles.length; i++){
+function mousePressed() {
+  for (let i = 0; i < rectiles.length; i++) {
+    rectiles.x = mouseX;
+    rectiles.y = mouseY;
+    console.log("rectiles")
+    
+    if ( == rectiles.length)
     
   }
 }
 
-
-//yunus, end screen fuction
-function gameOver(){
+//end screen fuction
+function gameOver() {
   noLoop();
   fill(255, 204, 100);
   textSize(48);
-  text('GAME OVER', width / 2, height / 2);
+  text("GAME OVER", width / 2, height / 2);
   textSize(12);
-  text('Press Reset Button to Restart', width / 4, height / 4);
+  text("Press Reset Button to Restart", width / 4, height / 4);
 }
 
-
-
-
+//tiles
 class Tile {
   constructor(width, height, x, y, velocity) {
     this.width = width;
@@ -64,16 +64,14 @@ class Tile {
     this.y = y;
     this.velocity = velocity;
   }
-  
-   move(){
+//tile movement
+  move() {
     this.y += this.velocity;
-    if (this.y > height){
+    if (this.y > height) {
       this.y = 0;
-    } 
     }
-  
-  
-  
+  }
+
   drawrectangle() {
     fill(0, 0, 0);
     rect(this.x, this.y, this.width, this.height);
